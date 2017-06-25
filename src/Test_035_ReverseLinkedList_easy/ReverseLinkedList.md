ca3a32b9fdecd401d1bf82081d4bd06258306f01
xiyouZmt <taominzhu@gmail.com>
2017-06-25 13:06:18 +0800

    add README and Code for each problem

---
+++ src/Test_035_ReverseLinkedList_easy/ReverseLinkedList.md
@@ -0,0 +1,41 @@
+## LintCode
+### 35. <a href="http://lintcode.com/problem/reverse-linked-list"> Reverse LinkedList (Easy) </a>
+	
+	package Test_035_ReverseLinkedList_easy;
+
+	/**
+	 * Created by zmt on 2017/6/24.
+	 */
+	public class ReverseLinkedList {
+	    public static void main(String [] args){
+	
+	    }
+	    public ListNode reverse(ListNode head){
+	        ListNode node = head;
+	        ListNode nextNode;
+	        ListNode preNode = null;
+	        ListNode reverseNode = null;
+	        while (node != null){
+	            nextNode = node.next;
+	            if (nextNode == null){
+	                reverseNode = node;
+	            }
+	            node.next = preNode;
+	            preNode = node;
+	            node = nextNode;
+	        }
+	        return reverseNode;
+	    }
+	}
+	
+	/**
+	 * Definition for ListNode.
+	 */
+	class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode(int val) {
+	        this.val = val;
+	        this.next = null;
+	    }
+	}
---
+++ src/Test_038_Search2DMatrix_medium/Search2DMatrix.java
@@ -0,0 +1,35 @@
+package Test_038_Search2DMatrix_medium;
+
+/**
+ * Created by zmt on 2017/6/24.
+ */
+public class Search2DMatrix {
+    public static void main(String [] args){
+        int [][] matrix = {
+                {1, 3, 5, 7},
+                {6, 7, 16, 20},
+                {7, 30, 34, 50}};
+        System.out.println(searchMatrix(matrix, 7));
+    }
+    public static int searchMatrix(int [][] matrix, int target){
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int count = 0;
+        int row = 0;
+        int column = matrix[row].length - 1;
+        while (row < matrix.length && column >= 0){
+            if(matrix[row][column] > target){
+                column--;
+            } else if(matrix[row][column] < target){
+                row ++;
+            } else {
+                count++;
+                column--;
+                row++;
+//                return true;
+            }
+        }
+        return count;
+    }
+}
