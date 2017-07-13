803e3a3caddefcf5d4cfef677a248cf2740473c3
xiyouZmt <taominzhu@gmail.com>
2017-07-13 14:46:19 +0800

    add some problem and README

---
+++ src/Test_375_CloneBinaryTree_easy/CloneBinaryTree.java
@@ -0,0 +1,45 @@
+package Test_375_CloneBinaryTree_easy;
+
+/**
+ * Created by zmt on 2017/7/13.
+ */
+public class CloneBinaryTree {
+    public static void main(String [] args){
+
+    }
+
+    public TreeNode cloneBinaryTree(TreeNode root){
+        if(root != null){
+            TreeNode cloneRoot = new TreeNode(root.val);
+            traversal(root, cloneRoot);
+            return cloneRoot;
+        }
+        return null;
+    }
+
+    public void traversal(TreeNode root, TreeNode cloneRoot){
+        if(root.left == null && root.right == null){
+            return;
+        }
+        if(root.left != null){
+            cloneRoot.left = new TreeNode(root.left.val);
+            traversal(root.left, cloneRoot.left);
+        }
+        if(root.right != null){
+            cloneRoot.right = new TreeNode(root.right.val);
+            traversal(root.right, cloneRoot.right);
+        }
+    }
+}
+
+/**
+ * Definition of TreeNode
+ */
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
